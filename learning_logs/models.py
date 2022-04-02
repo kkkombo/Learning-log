bb5334ab227bb0794580c4f3d97d9a0117032362
Alexandra Dedok <dedok.alexandra@gmail.com>
2022-04-02 20:53:01 +0200

    Add Topic class from Model

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+# Inheriting from Model class
 class Topic(models.Model):
     """A topic the user is learning about."""
     text = models.CharField(max_length = 200)
