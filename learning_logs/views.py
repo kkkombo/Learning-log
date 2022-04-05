9a2b2ac7f9a2f9194751a89eb8e4819b759b4d31
Alexandra Dedok <dedok.alexandra@gmail.com>
2022-04-05 10:33:40 +0200

    Fix template path for Windows

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 def index(request):
     """The home page for Learning Log."""
-    return render(request, 'learning_logs/index.html')
+    return render(request, 'learning_logs\index.html')
