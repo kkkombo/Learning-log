d4c30d3ef28912b4517071f9d036f059ea55781d
Alexandra Dedok <dedok.alexandra@gmail.com>
2022-04-09 01:37:11 +0200

    Fix owner problem, move from entry to topic

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -6,6 +6,7 @@ class Topic(models.Model):
     """A topic the user is learning about."""
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         """Return a string representation of the model."""
@@ -16,7 +17,6 @@ class Entry(models.Model):
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
         verbose_name_plural = 'entries'
