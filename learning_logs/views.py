1e02b2db97b03c00cda0bb5c1123910cf6639d66
Alexandra Dedok <dedok.alexandra@gmail.com>
2022-04-09 02:00:35 +0200

    Add current user check, refractor

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -20,9 +20,10 @@ def topics(request):
 def topic(request, topic_id):
     """Show a single topic and all its entries."""
     topic = Topic.objects.get(id=topic_id)
+
     # Make sure the topic belongs to the current user.
-    if topic.owner != request.user:
-        raise Http404
+    check_topic_owner(request, topic_id)
+
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
@@ -37,6 +38,9 @@ def new_topic(request):
         # POST data submitted; process data.
         form = TopicForm(data=request.POST)
         if form.is_valid():
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             form.save()
             return redirect('learning_logs:topics')
 
@@ -71,8 +75,7 @@ def edit_entry(request, entry_id):
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
     
-    if topic.owner != request.user:
-        raise Http404
+    check_topic_owner(request, topic_id=topic.id)
 
     if request.method != 'POST':
         # Initial request; pre-fill form with the current entry.
@@ -86,3 +89,9 @@ def edit_entry(request, entry_id):
 
     context = {'entry': entry, 'topic': topic, 'form': form}
     return render(request, 'learning_logs/edit_entry.html', context)
+
+def check_topic_owner(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+
+    if topic.owner != request.user:
+        raise Http404
