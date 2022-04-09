6771dde542b7235dc26dc8e5f73300c33a7bf128
Alexandra Dedok <dedok.alexandra@gmail.com>
2022-04-09 23:13:01 +0200

    I'm a dumbass

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -28,6 +28,7 @@ SECRET_KEY = env("SECRET_KEY")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+print(BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -73,9 +74,7 @@ ROOT_URLCONF = 'learning_log.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(BASE_DIR, 'learning_logs\templates')
-        ],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
